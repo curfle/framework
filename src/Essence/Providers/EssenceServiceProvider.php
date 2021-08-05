e6602f8bed0e239aa4ba7fa813f9bd07ca85680e
Julius Türich <info@joonlabs.com>
2021-08-06 01:55:13 +0200

    add basic service providers

---
+++ src/Essence/Providers/EssenceServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Curfle\Essence\Providers;
+
+use Curfle\Http\Request;
+use Curfle\Support\AggregateServiceProvider;
+use Curlfe\Support\Facades\URL;
+
+class EssenceServiceProvider extends AggregateServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        parent::register();
+    }
+}
---
+++ src/Essence/Support/Providers/RouteServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Curfle\Essence\Support\Providers;
+
+use Closure;
+use Curfle\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+
+    /**
+     * The callback that should be used to load the application's routes.
+     *
+     * @var Closure|null
+     */
+    protected ?Closure $loadRoutesUsing = null;
+
+    /**
+     * The RouteServiceProvider's namespace.
+     *
+     * @var string|null
+     */
+    protected ?string $namespace = null;
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->booted(function () {
+            $this->loadRoutes();
+        });
+    }
+
+    /**
+     * Register the callback that will be used to load the application's routes.
+     *
+     * @param Closure $routesCallback
+     * @return $this
+     */
+    protected function routes(Closure $routesCallback): static
+    {
+        $this->loadRoutesUsing = $routesCallback;
+        return $this;
+    }
+
+    /**
+     * Load the application routes.
+     *
+     * @return void
+     */
+    public function loadRoutes()
+    {
+        if (!is_null($this->loadRoutesUsing))
+            $this->app->call($this->loadRoutesUsing);
+    }
+
+}
