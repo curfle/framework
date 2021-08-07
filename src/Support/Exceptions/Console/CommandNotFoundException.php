f803307c130cd04c286fd5f67fb10cb77a303cd6
Julius Türich <info@joonlabs.com>
2021-08-07 19:24:00 +0200

    add console exceptions

---
+++ src/Support/Exceptions/Console/CommandNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Curfle\Support\Exceptions\Console;
+
+use Curfle\Support\Exceptions\CurfleException;
+
+class CommandNotFoundException extends CurfleException
+{
+
+}
---
+++ src/Support/Exceptions/Console/ConsoleException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Curfle\Support\Exceptions\Console;
+
+use Curfle\Support\Exceptions\CurfleException;
+
+class ConsoleException extends CurfleException
+{
+
+}
