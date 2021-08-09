bb8462504a86248678d452def134cfc31340ba01
Julius Türich <info@joonlabs.com>
2021-08-09 22:27:52 +0200

    change root password for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
  */
 const DB_HOST = "127.0.0.1";
 const DB_USER = "root";
-const DB_PASSWORD = "";
+const DB_PASSWORD = "root";
 const DB_DATABASE = "curfle";
 
 const DB_SQLITE_FILENAME = __DIR__ . "/Resources/Database/database.db";
