a7216c052ef7a148a02816e3770497d4440a7e44
Julius Türich <info@joonlabs.com>
2021-08-26 23:23:13 +0200

    add `Faker`

---
+++ src/Utilities/Faker/Faker.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Curfle\Utilities\Faker;
+
+class Faker
+{
+    /**
+     * Returns a random firstname.
+     *
+     * @return string
+     */
+    public static function firstname(): string
+    {
+        $names = ["Dario", "Wieland", "Felix", "Carolin", "Luna", "Niklas", "Leia", "Anna-Lena", "Johanna", "Lukas", "Charlotte", "Pauline", "Luise", "Justus", "Mathis", "Tristan", "Ben"];
+        return $names[rand(0, count($names) - 1)];
+    }
+
+    /**
+     * Returns a random lastname.
+     *
+     * @return string
+     */
+    public static function lastname(): string
+    {
+        $names = ["Larry", "Stapen", "Barrièr", "Dahle", "Mandle", "Frosch", "Spuli", "Qualis", "Mikrola", "Kett", "Tensit", "Erdal", "Kaisi", "Otto", "Protma", "Laurith", "Schmaurit", "Propül"];
+        return $names[rand(0, count($names) - 1)];
+    }
+
+    /**
+     * Returns a random full name.
+     *
+     * @return string
+     */
+    public static function name(): string
+    {
+        return self::firstname() . " " . self::lastname();
+    }
+
+    /**
+     * Returns a random email.
+     *
+     * @return string
+     */
+    public static function email(): string
+    {
+        $names = ["potassium", "sodium", "formic", "adipe", "cocamido", "carbona", "prunus", "victus", "dolciuo", "ami", "player", "dalus", "palm", "oleate", "gelbersack", "tucom"];
+        $domains = ["fixmail.dd", "supermail.dd", "badmail.dd", "middlemail.dd", "littlemail.dd", "sodamail.dd", "streammail.dd"];
+        return $names[rand(0, count($names) - 1)] . rand(1000, 10000) . "@" . $domains[rand(0, count($domains) - 1)];
+    }
+}
---
+++ tests/Utilities/FakerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Curfle\Tests\Utilities;
+
+use Curfle\Routing\Route;
+use Curfle\Utilities\Faker\Faker;
+use PHPUnit\Framework\TestCase;
+
+class FakerTest extends TestCase
+{
+    /**
+     * Tests the ::firstname() function.
+     */
+    public function testFirstname()
+    {
+        self::assertMatchesRegularExpression(
+            "/([A-Z]|[a-z])+/",
+            Faker::firstname()
+        );
+    }
+
+    /**
+     * Tests the ::lastname() function.
+     */
+    public function testLastname()
+    {
+        self::assertMatchesRegularExpression(
+            "/([A-Z]|[a-z])+/",
+            Faker::firstname()
+        );
+    }
+
+    /**
+     * Tests the ::email() function.
+     */
+    public function testEmail()
+    {
+        self::assertMatchesRegularExpression(
+            "/([A-Z]|[a-z])+([0-9]+)@([A-Z]|[a-z])+\.([A-Z]|[a-z])+/",
+            Faker::email()
+        );
+    }
+}
