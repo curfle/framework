7ff247cb1a882fe3b6c39d4ee7e362c2ed3cbf36
Julius Türich <info@joonlabs.com>
2021-08-30 12:03:05 +0200

    add `Chronos` for time management

---
+++ src/Chronos/Chronos.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Curfle\Chronos;
+
+use DateTimeZone;
+use Exception;
+
+class Chronos extends \DateTime
+{
+    /**
+     * Holds the timezone of the date.
+     *
+     * @var DateTimeZone|null
+     */
+    static ?DateTimeZone $timezone = null;
+
+    /**
+     * Set the new default timezone.
+     *
+     * @param string $timezone
+     * @return void
+     */
+    public static function useTimezone(string $timezone)
+    {
+        self::$timezone = new DateTimeZone($timezone);
+    }
+
+    /**
+     * Returns the current timestamp as Chronos object.
+     *
+     * @param string|null $timezone
+     * @return Chronos
+     * @throws Exception
+     */
+    public static function now(?string $timezone = null): Chronos
+    {
+        return static::parse("now", $timezone);
+    }
+
+    /**
+     * Returns the current timestamp as Chronos object.
+     *
+     * @param string $time
+     * @param string|null $timezone
+     * @return Chronos
+     * @throws Exception
+     */
+    public static function parse(string $time, ?string $timezone = null): Chronos
+    {
+        // parse timezone
+        if ($timezone !== null)
+            $timezone = new DateTimeZone($timezone);
+
+        return new Chronos($time, $timezone ?? self::$timezone);
+    }
+
+
+    /**
+     * Returns the year.
+     *
+     * @return int
+     */
+    public
+    function year(): int
+    {
+        return (int)$this->format("Y");
+    }
+
+    /**
+     * Returns the month.
+     *
+     * @return int
+     */
+    public
+    function month(): int
+    {
+        return (int)$this->format("m");
+    }
+
+    /**
+     * Returns the day.
+     *
+     * @return int
+     */
+    public
+    function day(): int
+    {
+        return (int)$this->format("d");
+    }
+
+    /**
+     * Returns the hour.
+     *
+     * @return int
+     */
+    public
+    function hour(): int
+    {
+        return (int)$this->format("H");
+    }
+
+    /**
+     * Returns the minute.
+     *
+     * @return int
+     */
+    public
+    function minute(): int
+    {
+        return (int)$this->format("i");
+    }
+
+    /**
+     * Returns the second.
+     *
+     * @return int
+     */
+    public
+    function second(): int
+    {
+        return (int)$this->format("s");
+    }
+
+    /**
+     * Returns the second.
+     *
+     * @return int
+     */
+    public
+    function microSecond(): int
+    {
+        return (int)$this->format("u");
+    }
+
+    /**
+     * Returns the second.
+     *
+     * @return int
+     */
+    public
+    function milliSecond(): int
+    {
+        return (int)$this->format("v");
+    }
+
+    /**
+     * Returns the weekDay.
+     *
+     * @return int
+     */
+    public
+    function weekDay(): int
+    {
+        return (int)$this->format("w");
+    }
+
+    /**
+     * Returns the year.
+     *
+     * @return int
+     */
+    public
+    function yearDay(): int
+    {
+        return (int)$this->format("z");
+    }
+
+    /**
+     * Returns the week.
+     *
+     * @return int
+     */
+    public
+    function week(): int
+    {
+        return (int)$this->format("W");
+    }
+
+    /**
+     * Returns wether the year is a leap year.
+     *
+     * @return bool
+     */
+    public
+    function isLeapYear(): bool
+    {
+        return (bool)$this->format("Y");
+    }
+}
---
+++ tests/Chronos/ChronosTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Curfle\Tests\Auth\JWT;
+
+
+use Curfle\Auth\JWT\JWT;
+use Curfle\Chronos\Chronos;
+use Curfle\Support\Env\Env;
+use Curfle\Support\Exceptions\Misc\SecretNotPresentException;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class ChronosTest extends TestCase
+{
+
+    /**
+     * Tests the ::parse() function.
+     * @throws Exception
+     */
+    public function testParse()
+    {
+        $chronos = Chronos::parse("2021-08-07 20:08:07", "EUROPE/Berlin");
+
+        $this->assertSame(2021, $chronos->year());
+        $this->assertSame(8, $chronos->month());
+        $this->assertSame(7, $chronos->day());
+        $this->assertSame(6, $chronos->weekDay());
+        $this->assertSame(218, $chronos->yearDay());
+        $this->assertSame(31, $chronos->week());
+        $this->assertSame(20, $chronos->hour());
+        $this->assertSame(8, $chronos->minute());
+        $this->assertSame(7, $chronos->second());
+        $this->assertSame(0, $chronos->milliSecond());
+        $this->assertSame(0, $chronos->microSecond());
+    }
+
+    /**
+     * Tests the ::parse() function.
+     * @throws Exception
+     */
+    public function testParseRelative()
+    {
+        date_default_timezone_set("EUROPE/Berlin");
+        $chronos = Chronos::parse("-1 minutes", "EUROPE/Berlin");
+
+        $this->assertSame((int)date("Y"), $chronos->year());
+        $this->assertSame((int)date("m"), $chronos->month());
+        $this->assertSame((int)date("d"), $chronos->day());
+        $this->assertSame((int)date("w"), $chronos->weekDay());
+        $this->assertSame((int)date("z"), $chronos->yearDay());
+        $this->assertSame((int)date("W"), $chronos->week());
+        $this->assertSame((int)date("H"), $chronos->hour());
+        $this->assertSame(((int)date("i")) - 1, $chronos->minute());
+        $this->assertSame((int)date("s"), $chronos->second());
+    }
+
+    /**
+     * Tests the ::parse() function.
+     * @throws Exception
+     */
+    public function testNow()
+    {
+        Chronos::useTimezone("EUROPE/Berlin");
+        date_default_timezone_set("EUROPE/Berlin");
+        $chronos = Chronos::now();
+
+        $this->assertSame((int)date("Y"), $chronos->year());
+        $this->assertSame((int)date("m"), $chronos->month());
+        $this->assertSame((int)date("d"), $chronos->day());
+        $this->assertSame((int)date("w"), $chronos->weekDay());
+        $this->assertSame((int)date("z"), $chronos->yearDay());
+        $this->assertSame((int)date("W"), $chronos->week());
+        $this->assertSame((int)date("H"), $chronos->hour());
+        $this->assertSame((int)date("i"), $chronos->minute());
+        $this->assertSame((int)date("s"), $chronos->second());
+    }
+}
