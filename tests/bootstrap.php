ff0c5317d0fb04aa3bccd1ec0c0c80fcdc426875
Julius Türich <info@joonlabs.com>
2021-10-20 14:08:52 +0200

    remove sqlite database from repo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,16 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 /**
- * Define DataBase Constants
+ * define database constants
  */
 const DB_HOST = "127.0.0.1";
 const DB_USER = "root";
 const DB_PASSWORD = "root";
 const DB_DATABASE = "curfle";
 
-const DB_SQLITE_FILENAME = __DIR__ . "/Resources/Database/database.db";
+const DB_SQLITE_FILENAME = __DIR__ . "/Resources/Database/database.sqlite";
+
+/**
+ * Create the .sqlite database file
+ */
+file_put_contents(DB_SQLITE_FILENAME, "");
