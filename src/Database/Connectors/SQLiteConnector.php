5eb05b55b873a0c6253cc133085717d9c0e78e62
Julius Türich <info@joonlabs.com>
2023-04-25 13:13:42 +0200

    remove unnecessary variable assignment

--- src/Database/Connectors/SQLiteConnector.php
+++ src/Database/Connectors/SQLiteConnector.php
@@ -158,7 +158,6 @@ class SQLiteConnector implements SQLConnectorInterface
      */
     function prepare(string $query): static
     {
-        $this->stmttxt = $query;
         $this->stmt = $this->connect()->prepare($query);
         return $this;
     }
